bca99a9fd1519f4d4a9554f4d13ebf4744d29b6a
MaheryRandrianirina <106425085+MaheryRandrianirina@users.noreply.github.com>
2023-09-08 09:35:03 +0300

    Fix css

--- resources/views/components/cvs/cv-3.blade.php
+++ resources/views/components/cvs/cv-3.blade.php
@@ -18,7 +18,7 @@
                     <svg class="icon phone-icon" viewBox="0 0 512 512">
                         <path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"/>
                     </svg>
-                    <p id="input" aria-name="phone_number" class="mb-0 ms-3">{{ isset($cv) ? NumberFormatter::phone("0" . $cv->contact->phone_number) : "Téléphone" }}</p>
+                    <p id="input" aria-type="number" aria-name="phone_number" class="mb-0 ms-3">{{ isset($cv) ? NumberFormatter::phone("0" . $cv->contact->phone_number) : "Téléphone" }}</p>
                 </div>
                 <div class="email-adress d-flex justify-content-start mb-2">
                     <svg class="icon email-icon" viewBox="0 0 512 512"><path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"/>
@@ -231,7 +231,7 @@
                     <div class="formation-{{ $stringNumber[$i] }}">
                         <div class="degree">
                             <h4 id="input" aria-name="degree_{{ $stringNumber[$i] }}">{{ $formation->degree }}</h4>
-                            <p aria-input-number='3' class="college">
+                            <p aria-input-number='3' class="date">
                                 <span id="input" aria-name="etablissement_{{ $stringNumber[$i] }}" class="etablissement">{{ $formation->etablissement }}</span> <span id="separator"> : </span> <span aria-input-number="2" class="date">
                                     <?php $splitted_date = mb_split(" - ",$formation->date); ?>
                                     <span id="input" aria-type="date" aria-name="year_debut_formation_{{ $stringNumber[$i] }}">{{ $formation->getDateDebut() }}</span> 
@@ -248,7 +248,7 @@
                 <div class="formation-one">
                     <div class="degree">
                         <h4 id="input" aria-name="degree_one">DIPLOME XXXXXXXXX</h4>
-                        <p aria-input-number='3' class="college">
+                        <p aria-input-number='3' class="date">
                             <span id="input" aria-name="etablissement_one" class="etablissement">Université ou école</span> <span id="separator"> : </span> <span aria-input-number="2" class="date">
                                 <span id="input" aria-type="date" aria-name="year_debut_formation_one">20XX</span> 
                                 <span id="separator">-</span> 
@@ -260,7 +260,7 @@
                 <div class="formation-two">
                     <div class="degree">
                         <h4 id="input" aria-name="degree_two">DIPLOME XXXXXXXXX</h4>
-                        <p aria-input-number='3' class="college">
+                        <p aria-input-number='3' class="date">
                             <span id="input" aria-name="etablissement_two" class="etablissement">Université ou école</span> <span id="separator"> : </span> <span aria-input-number="2" class="date">
                                 <span id="input" aria-type="date" aria-name="year_debut_formation_two">20XX</span> 
                                 <span id="separator">-</span> 
